de135b3bc137310a37213ac20be04b2ca4bdb6f2
ItsSandox <deriel.espinal@gmail.com>
2024-03-09 20:42:15 -0400

    Prueba label y boton
    - Agregar botones en el frame

--- client/gui_app.py
+++ client/gui_app.py
@@ -9,3 +9,23 @@ class App(tk.CTk):
         #self.iconbitmap('img\ini_sec.ico')
         self.geometry("540x500")
         self.config(bg='#124E78')
+        self.resizable(0, 0)
+        self.frame = Frame(master = self, bg_color='#001220', fg_color='#001220', width=540, height=500)
+        self.frame.place(x=0, y=0)
+
+
+class Frame(tk.CTkFrame):
+        def __init__(self, master, **kwargs):
+         super().__init__(master, **kwargs)
+         
+         self.boton1 = tk.CTkButton(self, text= "Hola Mundo", height=20, width= 30)
+         self.boton1.place(x=200, y=250)
+
+         self.boton2 = tk.CTkButton(self, text= "Adios Mundo", height=20, width= 30)
+         self.boton2.place(x=200, y=270)
+
+         self.titulo = tk.CTkLabel(self, text= "TITULO", fg_color= "transparent")
+         self.titulo.place(x=10, y=10)
+
+
+    
--- main.py
+++ main.py
@@ -8,4 +8,5 @@ def main():
     app.mainloop()
  
 if __name__ == '__main__':
-    main()
+    main() 
+
